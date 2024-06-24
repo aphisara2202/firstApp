d9e38191f733aa1f41bbc19ceff282f1a1265cd6
aphitsara2202 <aphisara.2546a@gmail.com>
2024-06-24 15:33:27 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {StyleSheet, View, Image} from 'react-native';
+
+const Flex = () => {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'column',
+        },
+      ]}>
+      <View style={{flex: 1, backgroundColor: 'red'}} >
+      <Image
+        source={require('./assets/lru.png')}
+      />
+      </View>
+      <View style={{flex: 2, backgroundColor: 'darkorange'}} />
+      <View style={{flex: 3, backgroundColor: 'green'}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
+
+export default Flex;
